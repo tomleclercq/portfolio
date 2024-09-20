35acd8714cf2630f83d7acd5d51afcf63872eb42
Tom Leclercq <tleclercq@deloitte.com>
2024-09-20 13:19:11 +0200

    update content

--- lib/pages/curriculum_vitae.dart
+++ lib/pages/curriculum_vitae.dart
@@ -20,11 +20,10 @@ class _CurriculumVitaeState extends State<CurriculumVitae> {
   @override
   Widget build(BuildContext context) {
     final display = ResponsiveDisplay(context);
-
     return Padding(
       padding: EdgeInsets.symmetric(
-        horizontal: 4 + display.x * 0.1,
-        vertical: 8 + display.y * 0.1,
+        horizontal: 4 + display.x * 0.05,
+        vertical: 8 + display.y * 0.05,
       ),
       child: const SizedBox(
         width: double.infinity,
@@ -32,8 +31,10 @@ class _CurriculumVitaeState extends State<CurriculumVitae> {
           child: Column(
             mainAxisSize: MainAxisSize.max,
             crossAxisAlignment: CrossAxisAlignment.start,
+            mainAxisAlignment: MainAxisAlignment.start,
             children: [
-              Center(
+              Padding(
+                padding: EdgeInsets.only(bottom: 32.0),
                 child: Text(
                   'Curriculum Vitae',
                   style: CustomStyles.h1,
@@ -42,10 +43,11 @@ class _CurriculumVitaeState extends State<CurriculumVitae> {
               Section(
                 title: 'Skills and Training',
                 listItems: [
-                  '<b>Font-End</b> Developer: <b>Flutter</b>, React-Native, native Android & iOS',
-                  'Developer Unity3D - behaviours, tools editor, plugin',
-                  '<b>Dart</b>, C#, C++, lua, the required language',
-                  'Image editing, <b>wireframe</b>, 2D/3D concepts, <b>User-Experience design</b>',
+                  '<b>Font-End</b> Developer: <b>Flutter</b>, React-Native, <b>React</b>, native Android & iOS',
+                  'Unity3D - behaviours, <b>tools editor</b>, plugin',
+                  '<b>Dart</b>, C#, C++, lua, any required language',
+                  'Image editing, <b>wireframe</b>, 2D/3D concepts, <b>UX design</b>',
+                  'Aug. 2024 Accessibility - Abra training',
                   'Mar. 2017 Structure and computer Network - Training',
                   'Mar. 2017 Administering Windows Server 2012 R2 - Training',
                 ],
@@ -53,36 +55,46 @@ class _CurriculumVitaeState extends State<CurriculumVitae> {
               Section(
                 title: 'Interests',
                 listItems: [
-                  '> Arts, Movies, Games, Music, <b>Science</b>, Socio-cultural interaction, <b>Technologies</b>.',
+                  'Movies, <b>Arts</b>, Games, Music, Travel, <b>Science</b>, Human interaction, <b>Technologies</b>.',
                 ],
               ),
               Section(
                 title: 'Memberships',
                 listItems: [
-                  '<b>Deloitte EMEA Metaverse</b> Community, Tech Lead',
-                  'Deloitte GLOBE, core-member & Open@Work',
-                  'Deloitte Diversity Equity & Inclusion, SPOC <b>LGBT+</b> consulting',
+                  '''Open@Work - Representative for Deloitte''',
+                  '''Deloitte Diversity Equity Inclusion:
+ + Proud - Core team
+ + <b>LGBT+</b> - Team Lead for Technology''',
+                  'Deloitte <b>EMEA Metaverse</b> - Community member',
                 ],
               ),
               Section(
                 title: 'Experiences',
                 listItems: [
-                  'Present <b>Senior Developer</b> Application developer',
-                  'Jan. 2022 <b>Senior Developer</b> Flutter application developer',
-                  'Jan. 2021 <b>Team lead</b> Flutter mobile application developer',
-                  'Jun. 2020 <b>Senior Specialist</b> - Mobile developer',
-                  'Mar. 2019 <b>Facilitator</b> Digital Masterclass Deloitte Belgium',
-                  'Mar. 2017 Blending Game Jam, Brussels (VR project Developer)',
-                  'Oct. 2015 Kiss Your Teacher (<b>Developer</b>)',
-                  'Jan. 2015 Global Game Jam, Antwerpen (<b>3D modelisation, Developer</b>)',
-                  'Jan. 2014 Global Game Jam, Antwerpen (Developer)',
-                  'Sept. 2009 - June 2013 <b>Bachelor</b> in Graphic Design, <b>specialised in Video Game Developement</b> - HEAJ, Namur.',
+                  '''Jan. 2021 - now <b>Lead</b>:
+ + Flutter mobile application developer
+ + Diversity, equity, inclusion: LGBT+ co-lead of consulting Deloitte, Belgium''',
+                  '''Jan. 2020 - now <b>Senior Developer</b>:
+ + Flutter application developer
+ + React application developer
+ + C# Unity3D Tool developer
+ + Mobile developer''',
+                  '''Mar. 2019 - now <b>Facilitator</b>:
+ + A11y workshops
+ + Inclusivity roundtables
+ + Knowledge-Sharing Sessions
+ + Digital Masterclass Deloitte Belgium''',
+                  '''Mar. 2017: Blending Game Jam, Brussels (VR project Developer)''',
+                  '''Oct. 2015: Kiss Your Teacher (<b>Developer</b>)''',
+                  '''Jan. 2015: Global Game Jam, Antwerpen (<b>3D modelisation, Developer</b>)''',
+                  '''Jan. 2014: Global Game Jam, Antwerpen (Developer)''',
+                  '''Sept. 2009 - June 2013 <b>Bachelor</b> as Graphic Designer, specialised in <b>Video Game Developement</b> - HEAJ, Namur.''',
                 ],
               ),
               Section(
                 title: 'Professional History',
                 listItems: [
-                  'Feb. 2018 - Present <b>Senior specialist, Front-end developer</b>, Deloitte Digital',
+                  'Feb. 2018 - Present <b>Senior specialist, Front-end developer</b>, Deloitte Digital, Belgium',
                   'Sept. 2013 - Jan. 2018 <b>Interactive Application Developer and Graphic designer</b>, Brandfirst',
                   'Dec. 2012 - Mar. 2013 Internship Developer C# - Brandfirst',
                 ],
